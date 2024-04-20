e9314cf837310026378e4a25c849cece2ba19432
Travis Taylor <ttaylor8@colostate.edu>
2024-04-20 12:03:02 -0600

    added code for server

--- Server/Server.h
+++ Server/Server.h
@@ -1,6 +1,8 @@
-#ifndef SERVER_H_INCLUDED
-#define SERVER_H_INCLUDED
+#ifndef SERVER_H
+#define SERVER_H
 
-int placeHolderServer();
+int receive_data(int sockfd, char* buffer, int buffer_size);
+int send_data(int sockfd, const char* data, int data_length);
+int handle_client(int client_sock);
 
-#endif /* SERVER_H_INCLUDED */
+#endif
--- Server/main.cc
+++ Server/main.cc
@@ -1,13 +1,68 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <vector>
 #include <string>
 #include <algorithm>
 #include <iomanip>
+#include <cstring> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "Server.h"
 
-using namespace std;
+#define SERVER_PORT 8080
 
 int main() {
-    return 0;
+  // Create a socket
+  int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_sock < 0) {
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Server address structure
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(SERVER_PORT);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+
+  // Bind the socket to the address
+  if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Listen for incoming connections
+  if (listen(server_sock, 3) < 0) {
+    perror("listen failed");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Server listening on port %d\n", SERVER_PORT);
+
+  // Accept client connections in a loop
+  while (1) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+    int client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_size);
+    if (client_sock < 0) {
+      perror("accept failed");
+      continue;
+    }
+
+    printf("Client connected from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    // Handle the client connection
+    if (handle_client(client_sock) < 0) {
+      perror("Error handling client");
+    }
+
+    // Close the client socket
+    close(client_sock);
+  }
+
+  // Close the server socket
+  close(server_sock);
+
+  return 0;
 }
