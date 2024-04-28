f30f3a5b434de37a3c4ae0a937aa2b75fa46e08e
Travis Taylor <ttaylor8@colostate.edu>
2024-04-28 15:29:12 -0600

    undo changes

--- Server/Server.cc
+++ Server/Server.cc
@@ -7,30 +7,22 @@
 #include "../Networking/Networking.h"
 #include "../Encryption/Encryption.h"
 
-#define SERVER_PORT 8080 
+
+#define SERVER_PORT 8080
 #define SERVER_SEND_PORT 8083
 using namespace std;
 
+
 int run() {
-  Networking net;
-  string key = "SUPER secret key";
-
-  // Receive data from the Raspberry Pi
-  string data = net.receive(SERVER_PORT);
-
-  // Decrypt the data
-  string decrypt;
-  Decrypt(data, decrypt, key);
-
-  // data processing here 
-  reverse(decrypt.begin(), decrypt.end()); 
-
-  // Encrypt the modified data
-  string send;
-  Encrypt(decrypt, send, key);
-
-  // Send the response back to the Raspberry Pi
-  if (net.send(SERVER_SEND_PORT, net.receive_ip, send) == -1) return -1;  
-
-  return 0;
+ Networking net;
+ string key = "SUPER secret key";
+ string data = net.receive(SERVER_PORT);
+ string decrypt;
+ Decrypt(data, decrypt, key);
+ cout << "Data from client: "<< decrypt << '\n';
+ decrypt[0] = '?'; // placeholder
+ string send;
+ Encrypt(decrypt,send,key);
+ if (net.send(SERVER_SEND_PORT, net.receive_ip, send) == -1) return -1;
+ return 0;
 }
